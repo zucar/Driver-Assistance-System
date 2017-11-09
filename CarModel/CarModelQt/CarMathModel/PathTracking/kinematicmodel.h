e9ca846d775fe730f431248d09ec95468a94fb10
Icarus <Mohamed.khaled.courses@gmail.com>
2017-11-09 16:34:54 +0200

    Added CarDiscription Class , Update QMAKE RULES

---
+++ CarModel/CarModelQt/CarMathModel/PathTracking/kinematicmodel.h
@@ -0,0 +1,46 @@
+#ifndef KINEMATICMODEL_H
+#define KINEMATICMODEL_H
+#include "cardescription.h"
+
+namespace Path {
+namespace KinematicModels {
+class DECLAR_LIB KinematicModel;
+}
+}
+class Path::KinematicModels::KinematicModel{
+    const double PI = 22.0 / 7;
+public:
+    virtual double longVelocity() const;
+    virtual double rotationalVelocity() const;
+    virtual double dX() const;
+    virtual double dY() const;
+    virtual double dTheta() const;
+    virtual double dPhie() const;
+    virtual double toDegree(const double& rad) const;
+    CarDescription *getCarDesc() const;
+    void setCarDesc(CarDescription *value);
+
+    double getAngularVL() const;
+    void setAngularVL(double value);
+
+    double getAngularVR() const;
+    void setAngularVR(double value);
+
+    double getTheta() const;
+    void setTheta(double value);
+
+    double getU1() const;
+    void setU1(double value);
+
+    double getU2() const;
+    void setU2(double value);
+
+private:
+    CarDescription *carDesc;
+    double angularVL;
+    double angularVR;
+    double u1, theta, u2;
+protected:
+};
+
+#endif // KINEMATICMODEL_H
